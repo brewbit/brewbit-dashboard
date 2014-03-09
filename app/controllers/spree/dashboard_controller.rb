b7dc324dd553adcd17d4dcaf897a005827a0891c
Nick Hebner <hebnern@gmail.com>
2014-03-08 19:30:33 -0800

    Add base dashboard controller and view

---
+++ app/controllers/spree/dashboard_controller.rb
@@ -0,0 +1,6 @@
+module Spree
+  class DashboardController < Spree::StoreController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  # Add your extension routes here
+  get '/dashboard' => 'dashboard#index'
 end
