f49ac6617c54d6be99d7bda26afba8679241a92b
Nick Hebner <hebnern@gmail.com>
2014-03-09 19:34:10 -0700

    Redirect dashboard root to devices index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Spree::Core::Engine.routes.draw do
   scope 'dashboard' do
+    get '/' => 'devices#index'
     get '/devices/activate' => 'devices#start_activate'
     post '/devices/activate' => 'devices#activate'
     resources :devices
-  
-    get '/' => 'dashboard#index'
   end
 end
