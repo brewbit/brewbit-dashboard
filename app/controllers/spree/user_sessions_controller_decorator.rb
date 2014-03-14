770731126b8f20b09cfb6a1c8a3aed76f90e0763
Nick Hebner <hebnern@gmail.com>
2014-03-13 21:14:57 -0700

    Direct admin users to the admin dashboard.

--- app/controllers/spree/user_sessions_controller_decorator.rb
+++ app/controllers/spree/user_sessions_controller_decorator.rb
@@ -1,5 +1,9 @@
 Spree::UserSessionsController.class_eval do
   def after_sign_in_path_for(user)
-    '/dashboard'
+    if user.has_spree_role? 'admin'
+      '/admin'
+    else
+      '/dashboard'
+    end
   end
 end
