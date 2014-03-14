a9fc4222f06112c3dd23b12c5dea8ff79ebc4428
Nick Hebner <hebnern@gmail.com>
2014-03-13 19:23:02 -0700

    Set default role for new user to "user".

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,11 @@
 module Spree
   User.class_eval do
+    after_create :set_default_role
+    
     has_many :devices, class_name: "Devices"
+    
+    def set_default_role
+      self.spree_roles << Spree::Role.find_or_create_by(name: "user")
+    end
   end
 end
