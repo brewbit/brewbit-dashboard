85f21c5639fa3e53fc93e107ea874059e7d48c22
Nick Hebner <hebnern@gmail.com>
2014-03-25 21:37:34 -0700

    Move device settings notification into device commands controller.

--- app/controllers/spree/device_commands_controller.rb
+++ app/controllers/spree/device_commands_controller.rb
@@ -1,12 +1,14 @@
+require 'protobuf_messages/messages'
+
 module Spree
   class DeviceCommandsController < Spree::StoreController
     layout 'spree/layouts/devices'
     before_action :set_device
-    before_action :set_device_command, only: [:show, :edit, :update, :destroy]
+    before_action :set_device_command, only: [:show, :edit, :destroy]
   
     # GET /commands
     def index
-      @device_commands = DeviceCommand.all
+      @device_commands = @device.commands
     end
   
     # GET /commands/1
@@ -15,9 +17,7 @@ module Spree
   
     # GET /commands/new
     def new
-      logger.warn @device
       @device_command = Defaults.build_device_command @device, false
-      logger.warn @device_command
     end
   
     # GET /commands/1/edit
@@ -29,18 +29,10 @@ module Spree
       @device_command = DeviceCommand.new(device_command_params)
   
       if @device_command.save
-        redirect_to @device_command, notice: 'Device command was successfully created.'
+        notify_device_with_new_settings
+        redirect_to @device, notice: 'Device command was successfully sent.'
       else
         render action: 'new'
-      end
-    end
-  
-    # PATCH/PUT /commands/1
-    def update
-      if @device_command.update(device_command_params)
-        redirect_to @device_command, notice: 'Device command was successfully updated.'
-      else
-        render action: 'edit'
       end
     end
   
@@ -59,10 +51,69 @@ module Spree
       def set_device
         @device = Device.find(params[:device_id])
       end
-  
+      
+      def notify_device_with_new_settings
+        connection = DeviceConnection.find_by_device_id( @device.hardware_identifier )
+
+        # no need to send settings to a device that's not connected
+        unless connection
+          logger.warn "Device not connected during settings update #{@device.hardware_identifier}"
+          logger.warn "Connected devices are: #{DeviceConnection.all.inspect}"
+          return
+        end
+
+        data = {
+          outputs: [],
+          sensors: [],
+          temp_profiles: []
+        }
+        @device.outputs.each do |o|
+          output = {
+            index:            o.output_index,
+            function:         o.function,
+            cycle_delay:      o.cycle_delay,
+            sensor_index:     o.sensor.sensor_index,
+            output_mode:      o.output_mode
+          }
+          data[:outputs] << output
+        end
+        @device.sensors.each do |s|
+          sensor = {
+            index:            s.sensor_index,
+            setpoint_type:    s.setpoint_type
+          }
+          case s.setpoint_type
+          when Sensor::SETPOINT_TYPE[:static]
+            sensor[:static_setpoint] = s.static_setpoint
+          when Sensor::SETPOINT_TYPE[:temp_profile]
+            sensor[:temp_profile_id] = s.temp_profile_id
+            temp_profile = {
+              id:           s.temp_profile.id,
+              name:         s.temp_profile.name,
+              start_value:  s.temp_profile.start_value,
+              steps:        s.temp_profile.steps.collect { |step| {
+                  duration: step.duration,
+                  value:    step.value,
+                  type:     step.step_type
+                }
+              }
+            }
+            data[:temp_profiles] << temp_profile
+          end
+          data[:sensors] << sensor
+        end
+
+        type = ProtobufMessages::ApiMessage::Type::DEVICE_SETTINGS_NOTIFICATION
+        message = ProtobufMessages::Builder.build( type, data )
+        logger.debug "Sending Device Settings Notification Message: #{message.inspect}"
+        ProtobufMessages::Sender.send( message, connection )
+      end
+
       # Only allow a trusted parameter "white list" through.
       def device_command_params
-        params[:device_command]
+        params.require(:device_command).permit(:name,
+          output_settings_attributes: [:id, :function, :cycle_delay, :sensor_settings_id, :output_mode],
+          sensor_settings_attributes: [:id, :setpoint_type, :static_setpoint, :temp_profile_id] )
       end
   end
 end
--- app/controllers/spree/devices_controller.rb
+++ app/controllers/spree/devices_controller.rb
@@ -1,5 +1,3 @@
-require 'protobuf_messages/messages'
-
 module Spree
   class DevicesController < Spree::StoreController
     layout :resolve_layout
@@ -38,7 +36,6 @@ module Spree
     # PATCH/PUT /devices/1
     def update
       if @device.update(device_params)
-        notify_device_with_new_settings
         redirect_to @device, notice: 'Device was successfully updated.'
       else
         render action: 'edit'
@@ -54,70 +51,11 @@ module Spree
     private
       # Only allow a trusted parameter "white list" through.
       def device_params
-        params.require(:device).permit(:name,
-          outputs_attributes: [:id, :function, :cycle_delay, :sensor_id, :output_mode],
-          sensors_attributes: [:id, :setpoint_type, :static_setpoint, :temp_profile_id] )
-      end
-
-      def notify_device_with_new_settings
-        connection = DeviceConnection.find_by_device_id( @device.hardware_identifier )
-
-        # no need to send settings to a device that's not connected
-        unless connection
-          logger.warn "Device not connected during settings update #{@device.hardware_identifier}"
-          logger.warn "Connected devices are: #{DeviceConnection.all.inspect}"
-          return
-        end
-
-        data = {
-          outputs: [],
-          sensors: [],
-          temp_profiles: []
-        }
-        @device.outputs.each do |o|
-          output = {
-            index:            o.output_index,
-            function:         o.function,
-            cycle_delay:      o.cycle_delay,
-            sensor_index:     o.sensor.sensor_index,
-            output_mode:      o.output_mode
-          }
-          data[:outputs] << output
-        end
-        @device.sensors.each do |s|
-          sensor = {
-            index:            s.sensor_index,
-            setpoint_type:    s.setpoint_type
-          }
-          case s.setpoint_type
-          when Sensor::SETPOINT_TYPE[:static]
-            sensor[:static_setpoint] = s.static_setpoint
-          when Sensor::SETPOINT_TYPE[:temp_profile]
-            sensor[:temp_profile_id] = s.temp_profile_id
-            temp_profile = {
-              id:           s.temp_profile.id,
-              name:         s.temp_profile.name,
-              start_value:  s.temp_profile.start_value,
-              steps:        s.temp_profile.steps.collect { |step| {
-                  duration: step.duration,
-                  value:    step.value,
-                  type:     step.step_type
-                }
-              }
-            }
-            data[:temp_profiles] << temp_profile
-          end
-          data[:sensors] << sensor
-        end
-
-        type = ProtobufMessages::ApiMessage::Type::DEVICE_SETTINGS_NOTIFICATION
-        message = ProtobufMessages::Builder.build( type, data )
-        logger.debug "Sending Device Settings Notification Message: #{message.inspect}"
-        ProtobufMessages::Sender.send( message, connection )
+        params.require(:device).permit(:name)
       end
 
       def correct_user
-        @device = spree_current_user.devices.find_by( id: params[:id] )
+        @device = spree_current_user.devices.find( params[:id] )
         redirect_to root_path, error: 'You can only see your own devices' unless @device
       end
       
