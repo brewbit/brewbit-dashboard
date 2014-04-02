a0cd883f5d1567fe906dd9f2e17cd5269f81c2e8
Nick Hebner <hebnern@gmail.com>
2014-04-01 22:42:17 -0700

    Add device command history views.

--- app/models/device_command.rb
+++ app/models/device_command.rb
@@ -16,7 +16,7 @@ class DeviceCommand < ActiveRecord::Base
   has_many :sensor_settings, class_name: 'SensorSettings', dependent: :destroy
   has_many :output_settings, class_name: 'OutputSettings', dependent: :destroy
 
-  validates :name, length: { maximum: 100 }
+  validates :name, presence: true, length: { maximum: 100 }
   
   accepts_nested_attributes_for :sensor_settings, :output_settings
 end
