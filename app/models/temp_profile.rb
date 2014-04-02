7e75add28889e00ce67eb2425c7e1cb14f0e7642
Nick Hebner <hebnern@gmail.com>
2014-04-02 01:58:50 -0700

    Conver temp_profile.start_val to the selected temp scale.

--- app/models/temp_profile.rb
+++ app/models/temp_profile.rb
@@ -13,4 +13,40 @@ class TempProfile < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 100 }
+  
+  def start_value
+    scale = self.try( :user ).try( :temperature_scale )
+
+    case scale
+    when 'F'
+      read_attribute( :start_value )
+    when 'C'
+      fahrenheit_to_celcius( read_attribute(:start_value) )
+    else
+      read_attribute( :start_value )
+    end
+  end
+
+  def start_value=(val)
+    scale = self.try( :user ).try( :temperature_scale )
+
+    case scale
+    when 'F'
+      write_attribute( :start_value, val )
+    when 'C'
+      write_attribute( :start_value, celcius_to_fahrenheit(val) )
+    else
+      write_attribute( :start_value, val )
+    end
+  end
+
+  private
+
+  def fahrenheit_to_celcius(degrees)
+    (degrees.to_f - 32) / 1.8
+  end
+
+  def celcius_to_fahrenheit(degrees)
+    degrees.to_f * 1.8 + 32
+  end
 end
