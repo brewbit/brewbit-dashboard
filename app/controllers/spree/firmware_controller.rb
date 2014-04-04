ea32af1dac7eef040bbb3eba29138c5ac1649ec9
Misha Manulis <misha@manulis.com>
2014-04-04 00:19:19 -0700

    Fix autoloader failing to include firmware module

--- app/controllers/spree/firmware_controller.rb
+++ app/controllers/spree/firmware_controller.rb
@@ -1,50 +1,54 @@
-class Spree::FirmwareController < Spree::Admin::BaseController
+require 'firmware'
 
-  class FirmwareParams
-    def self.build( params )
-      params.require( :firmware ).permit( :version, :file )
-    end
-  end
+module Spree
+  class FirmwareController < Spree::Admin::BaseController
 
-  def index
-    @firmware_list = Spree::Firmware.order( 'version DESC' )
-  end
-
-  def new
-    @firmware = Spree::Firmware.new
-  end
-
-  def create
-    if params[:firmware][:file]
-      file = params[:firmware][:file]
-      params[:firmware].delete :file
+    class FirmwareParams
+      def self.build( params )
+        params.require( :firmware ).permit( :version, :file )
+      end
     end
 
-    @firmware = Spree::Firmware.new( FirmwareParams.build(params) ) do |t|
-      t.file = file.read if file
+    def index
+      @firmware_list = Spree::Firmware.order( 'version DESC' )
     end
 
-    if @firmware.save
+    def new
+      @firmware = Spree::Firmware.new
+    end
+
+    def create
+      if params[:firmware][:file]
+        file = params[:firmware][:file]
+        params[:firmware].delete :file
+      end
+
+      @firmware = Spree::Firmware.new( FirmwareParams.build(params) ) do |t|
+        t.file = file.read if file
+      end
+
+      if @firmware.save
+        redirect_to firmware_index_path
+      else
+        render action: :new
+      end
+    end
+
+    def destroy
+      @firmware = Spree::Firmware.find params[:id]
+      @firmware.destroy
       redirect_to firmware_index_path
-    else
-      render action: :new
     end
-  end
 
-  def destroy
-    @firmware = Spree::Firmware.find params[:id]
-    @firmware.destroy
-    redirect_to firmware_index_path
-  end
+    def serve
+      @firmware = Spree::Firmware.find params[:id]
+      send_data @firmware.file, type: 'application/octet-stream', filename: file_name, disposition: 'inline'
+    end
 
-  def serve
-    @firmware = Spree::Firmware.find params[:id]
-    send_data @firmware.file, type: 'application/octet-stream', filename: file_name, disposition: 'inline'
-  end
+    private
 
-  private
-
-  def file_name
-    "app_mt_update-#{@firmware.version}.bin"
+    def file_name
+      "app_mt_update-#{@firmware.version}.bin"
+    end
   end
 end
