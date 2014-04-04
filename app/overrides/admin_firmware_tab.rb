140fe45fdedda9629ef128220883be732eeeb1ee
Nick Hebner <hebnern@gmail.com>
2014-04-04 00:56:57 -0700

    Improve styling of admin tabs.

--- app/overrides/admin_firmware_tab.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "spree/admin/shared/_menu",
-  :name => 'firmware-tab',
-  :insert_bottom => "[data-hook='admin_tabs']",
-  :text => "<%= tab(:firmware, url: firmware_index_path) %>"
-)
