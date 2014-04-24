f6d196847091f5474fbacd435cbecbddd92cd573
Nick Hebner <hebnern@gmail.com>
2014-04-24 08:36:31 -0700

    Check user before using it.

--- app/controllers/spree/discourse_sso_controller.rb
+++ app/controllers/spree/discourse_sso_controller.rb
@@ -2,15 +2,17 @@ module Spree
   class DiscourseSsoController < Spree::StoreController
     def sso
       user = spree_current_user
-      redirect_to login_path if user.nil?
-      
-      sso = SingleSignOn.parse(request.query_string)
-      
-      sso.email = user.email
-      sso.username = user.login
-      sso.external_id = user.id
-  
-      redirect_to sso.to_url
+      if user
+        sso = SingleSignOn.parse(request.query_string)
+        
+        sso.email = user.email
+        sso.username = user.login
+        sso.external_id = user.id
+    
+        redirect_to sso.to_url
+      else
+        redirect_to login_path
+      end
     end
   end
 end
