6a10c0a2904a804db27997fbfdec7b516752ad29
Nick Hebner <hebnern@gmail.com>
2015-02-12 23:45:59 -0800

    Fix temp profile session uploading

--- app/controllers/brewbit/api/v1/controller_settings_controller.rb
+++ app/controllers/brewbit/api/v1/controller_settings_controller.rb
@@ -5,13 +5,14 @@ module Brewbit
         respond_to :json
 
         def create
-          session_action = params.delete(:session_action) || 1
-          params[:name] = "Device-Initiated Session" if params[:name].blank?
+          sp = session_params
+          session_action = sp.delete(:session_action) || 1
+          sp[:name] = "Device-Initiated Session" if sp[:name].blank?
 
           if session_action == 0 # create new session
-            session = create_session( @device, session_params )
+            session = create_session( @device, sp )
           else # update current session
-            session = update_session( @device, session_params )
+            session = update_session( @device, sp )
           end
 
           if session.save
@@ -41,27 +42,8 @@ module Brewbit
             current_session.save!
           end
 
-          device_session_params = {
-            name: settings[:name],
-            device_id: device.id,
-            sensor_index: settings[:sensor_index],
-            setpoint_type: settings[:setpoint_type],
-            static_setpoint: settings[:static_setpoint],
-            temp_profile_id: settings[:temp_profile_id],
-            output_settings_attributes: []
-          }
-
-          unless settings[:output_settings].nil?
-            settings[:output_settings].each do |output_setting|
-              device_session_params[:output_settings_attributes] << {
-                output_index: output_setting[:index],
-                function: output_setting[:function],
-                cycle_delay: output_setting[:cycle_delay]
-              }
-            end
-          end
-
-          device_session = DeviceSession.new(device_session_params)
+          device_session = DeviceSession.new(settings)
+          device_session.device = device
           device_session.active = true
 
           device_session
@@ -79,8 +61,8 @@ module Brewbit
               params.require(:temp_profile_id)
             end
 
-            unless params[:output_settings].nil?
-              params[:output_settings].each do |output_setting|
+            unless params[:output_settings_attributes].nil?
+              params[:output_settings_attributes].each do |output_setting|
                 validate_output_setting( output_setting )
               end
             end
@@ -89,12 +71,12 @@ module Brewbit
             @message = e.message
             render :error, status: 400
           else
-            params.permit(:session_action, :name, :sensor_index, :setpoint_type, :static_setpoint, :temp_profile_id, output_settings: [ :index, :function, :cycle_delay ])
+            params.require(:controller_setting).except(:auth_token, :device_id).permit(:session_action, :name, :sensor_index, :setpoint_type, :static_setpoint, :temp_profile_id, :temp_profile_completion_action, :temp_profile_start_point, output_settings_attributes: [ :output_index, :function, :cycle_delay ])
           end
         end
 
         def validate_output_setting( output_setting )
-          output_setting.require(:index)
+          output_setting.require(:output_index)
           output_setting.require(:function)
           output_setting.require(:cycle_delay)
         end
